e04f301bd45b7364aebcfa48f58550e1924eb159
Eth3R-One <refaythii@gmail.com>
2024-01-18 21:34:16 +0600

    feat: Vercel Web Analytics

--- script/app.js
+++ script/app.js
@@ -1,37 +1,39 @@
-import data from '../projects.json' ;
+import data from "../projects.json";
+import { inject } from "@vercel/analytics";
 
-function get_random_quote(){
-    return data.quotes[Math.floor(Math.random() * data.quotes.length)];
+inject();
+function get_random_quote() {
+  return data.quotes[Math.floor(Math.random() * data.quotes.length)];
 }
 
 const quote = get_random_quote();
-document.getElementById("quote").innerHTML =`
+document.getElementById("quote").innerHTML = `
     ${quote.en} <br />
     - ${quote.author}
 `;
 const softSkills = data.skills.soft;
 // console.log(softSkills);
-softSkills.forEach((skill)=>{
-    const list = document.createElement("li");
-    list.innerText = skill;
-    document.getElementById("soft-skill").appendChild(list);
+softSkills.forEach((skill) => {
+  const list = document.createElement("li");
+  list.innerText = skill;
+  document.getElementById("soft-skill").appendChild(list);
 });
 
 const syntax = data.skills.syntax;
 
-syntax.forEach((lan)=>{
-    const list = document.createElement("li");
-    list.innerText = lan;
-    document.getElementById("syntax").appendChild(list);
+syntax.forEach((lan) => {
+  const list = document.createElement("li");
+  list.innerText = lan;
+  document.getElementById("syntax").appendChild(list);
 });
 
 const projects = data.projects;
 
-projects.forEach(project => {
-    const external_link = "./assets/external-link-0ae89760.svg";
-    const repo_inner_div = document.createElement("div");
-    repo_inner_div.classList.add("repository-head");
-    repo_inner_div.innerHTML = `
+projects.forEach((project) => {
+  const external_link = "./assets/external-link-0ae89760.svg";
+  const repo_inner_div = document.createElement("div");
+  repo_inner_div.classList.add("repository-head");
+  repo_inner_div.innerHTML = `
     <a
     href="${project.link}"
     target="_blank"
@@ -40,7 +42,7 @@ projects.forEach(project => {
     <h3>${project.title}</h3>
   </a>
     `;
-    repo_inner_div.innerHTML += `<a
+  repo_inner_div.innerHTML += `<a
     href="${project.link}"
     target="_blank"
     rel="Refayth Hossain"
@@ -52,21 +54,21 @@ projects.forEach(project => {
       alt="external-link"
     />
   </a>`;
-    const repo = document.createElement("div");
-    repo.classList.add("repository-container");    
-    repo.appendChild(repo_inner_div);
-    const para = document.createElement("p");
-    para.classList.add("repository-description");
-    para.innerText =project.description;
-    repo.appendChild(para)
-    const lang = document.createElement("div");
-    lang.classList.add("repository-language-used-container");
-    project.language.forEach((syntax)=>{
-        const language_span = document.createElement("span");
-        language_span.classList.add("language-used");
-        language_span.innerText = syntax;
-        lang.appendChild(language_span);
-    });
-    repo.appendChild(lang);
-    document.getElementById("project-list").appendChild(repo);
+  const repo = document.createElement("div");
+  repo.classList.add("repository-container");
+  repo.appendChild(repo_inner_div);
+  const para = document.createElement("p");
+  para.classList.add("repository-description");
+  para.innerText = project.description;
+  repo.appendChild(para);
+  const lang = document.createElement("div");
+  lang.classList.add("repository-language-used-container");
+  project.language.forEach((syntax) => {
+    const language_span = document.createElement("span");
+    language_span.classList.add("language-used");
+    language_span.innerText = syntax;
+    lang.appendChild(language_span);
+  });
+  repo.appendChild(lang);
+  document.getElementById("project-list").appendChild(repo);
 });
