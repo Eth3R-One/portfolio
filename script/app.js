eba2f638c2da66fbbc0b620da352f8fde5a395c9
Eth3R-One <refaythii@gmail.com>
2025-04-12 10:40:30 +0600

    fix: link

--- script/app.js
+++ script/app.js
@@ -1,7 +1,7 @@
 import data from "../projects.json";
 
 function get_random_quote() {
-	return data.quotes[Math.floor(Math.random() * data.quotes.length)];
+  return data.quotes[Math.floor(Math.random() * data.quotes.length)];
 }
 
 const quote = get_random_quote();
@@ -12,36 +12,36 @@ document.getElementById("quote").innerHTML = `
 const softSkills = data.skills.soft;
 // console.log(softSkills);
 softSkills.forEach((skill) => {
-	const list = document.createElement("li");
-	list.innerText = skill;
-	document.getElementById("soft-skill").appendChild(list);
+  const list = document.createElement("li");
+  list.innerText = skill;
+  document.getElementById("soft-skill").appendChild(list);
 });
 
 const syntax = data.skills.syntax;
 
 syntax.forEach((lan) => {
-	const list = document.createElement("li");
-	list.innerText = lan;
-	document.getElementById("syntax").appendChild(list);
+  const list = document.createElement("li");
+  list.innerText = lan;
+  document.getElementById("syntax").appendChild(list);
 });
 
 const projects = data.projects;
 
 projects.forEach((project) => {
-	const external_link = "./assets/external-link-0ae89760.svg";
-	const repo_inner_div = document.createElement("div");
-	repo_inner_div.classList.add("repository-head");
-	repo_inner_div.innerHTML = `
+  const external_link = "./assets/external-link-0ae89760.svg";
+  const repo_inner_div = document.createElement("div");
+  repo_inner_div.classList.add("repository-head");
+  repo_inner_div.innerHTML = `
     <a
-    href="${project.git_link}"
+    href="${project.link}"
     target="_blank"
     rel="Refayth Hossain"
   >
     <h3>${project.title}</h3>
   </a>
     `;
-	repo_inner_div.innerHTML += `<a
-    href="${project.git_link}"
+  repo_inner_div.innerHTML += `<a
+    href="${project.link}"
     target="_blank"
     rel="Refayth Hossain"
     >
@@ -54,31 +54,31 @@ projects.forEach((project) => {
   </a>
 
   `;
-	// ${project?.live_link ? `<a href="${project.live_link}">Live</a>` : ""}
-	if (project.live_link) {
-		repo_inner_div.innerHTML += `|<a
+  // ${project?.live_link ? `<a href="${project.live_link}">Live</a>` : ""}
+  if (project.live_link) {
+    repo_inner_div.innerHTML += `|<a
     href="${project.live_link}"
     target="_blank"
     rel="Refayth Hossain"
     >
     Live
   </a>`;
-	}
-	const repo = document.createElement("div");
-	repo.classList.add("repository-container");
-	repo.appendChild(repo_inner_div);
-	const para = document.createElement("p");
-	para.classList.add("repository-description");
-	para.innerText = project.description;
-	repo.appendChild(para);
-	const lang = document.createElement("div");
-	lang.classList.add("repository-language-used-container");
-	project.language.forEach((syntax) => {
-		const language_span = document.createElement("span");
-		language_span.classList.add("language-used");
-		language_span.innerText = syntax;
-		lang.appendChild(language_span);
-	});
-	repo.appendChild(lang);
-	document.getElementById("project-list").appendChild(repo);
+  }
+  const repo = document.createElement("div");
+  repo.classList.add("repository-container");
+  repo.appendChild(repo_inner_div);
+  const para = document.createElement("p");
+  para.classList.add("repository-description");
+  para.innerText = project.description;
+  repo.appendChild(para);
+  const lang = document.createElement("div");
+  lang.classList.add("repository-language-used-container");
+  project.language.forEach((syntax) => {
+    const language_span = document.createElement("span");
+    language_span.classList.add("language-used");
+    language_span.innerText = syntax;
+    lang.appendChild(language_span);
+  });
+  repo.appendChild(lang);
+  document.getElementById("project-list").appendChild(repo);
 });
